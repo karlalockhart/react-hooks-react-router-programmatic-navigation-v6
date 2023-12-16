98dc2b2653c0494bd9d48757ebeff62e97ca361d
Karla Lockhart <kblockhart@gmail.com>
2023-12-16 11:06:09 -0800

    updated files

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,14 @@
 import { useState } from "react";
+import { useOutletContext } from "react-router-dom";
 
 function Login() {
+  // Access the login function passed as context
+  const login = useOutletContext();
   const [formData, setFormData] = useState({
     username: "",
     password: "",
   });
+  console.log('navigated to login')
 
   function handleChange(e) {
     setFormData({
@@ -12,34 +16,28 @@ function Login() {
       [e.target.name]: e.target.value,
     });
   }
-
+  // Create a function that calls the login function when the form is submitted
   function handleLogin(e) {
     e.preventDefault();
+    login();
   }
 
   return (
     <form onSubmit={handleLogin}>
-      <label for="username">Username</label>
-      <div>
-        <input
-          id="username"
-          type="text"
-          name="username"
-          value={formData.username}
-          onChange={handleChange}
-        />
-      </div>
-      <label for="password">Password</label>
-      <div>
-        <input
-          id="password"
-          type="password"
-          name="password"
-          value={formData.password}
-          onChange={handleChange}
-        />      
-      </div>
-      <button type="submit">Login</button>
+      <h1>Login</h1>
+      <input
+        type="text"
+        name="username"
+        value={formData.username}
+        onChange={handleChange}
+      />
+      <input
+        type="password"
+        name="password"
+        value={formData.password}
+        onChange={handleChange}
+      />
+      <input type="submit" value="Login"/>
     </form>
   );
 }
